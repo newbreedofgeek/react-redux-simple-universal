385417e45b6771bb651796669469441d2c35df66
newbreedofgeek <paul.mark@gmail.com>
2017-01-09 13:04:55 +1100

    added babel-polyfill and isomorphic-fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-require('./client.js');
+// bring in babel-polyfill as we need a Promise polyfill to ensure our isomorphic-fetch works the same across server and all browsers.
+import 'babel-polyfill';
+
+// load the client app.
+import './client.js';
